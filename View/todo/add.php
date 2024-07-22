04ffb2a7c4a7264d403f157d517e41d74662b5c6
Saliha Kotan <salihakotan77@gmail.com>
2024-07-23 02:03:22 +0300

    adding todos, sweet alert 2, Axios

---
+++ View/todo/add.php
@@ -0,0 +1,170 @@
+<?php view('static/header') ?>
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Navbar Search -->
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="<?= URL.'cikis';?>" class="nav-link">Çıkış yap</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+   <?= view('static/sidebar')?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper p-5">
+
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Yapılacaklar listenize ekleyin</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <?php
+                            echo get_session('error') ? '<div class="alert alert-'.$_SESSION['error']['type'].'">'.$_SESSION['error']['message'].'</div>': null;
+                            ?>
+                            <form id="todo" action="" method="post">
+                                <div class="card-body">
+
+                                    <div class="form-group">
+                                        <label for="title">Kategori seçiniz</label>
+                                        <select id="category_id" class="form-control">
+                                            <option value="0">- Kategori seçimi yapınız -</option>
+                                            <?php foreach ($data as $category): ?>
+                                            <option value="<?= $category['id']?>"><?= $category['title']?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="title">Başlık</label>
+                                        <input type="text" name="title" class="form-control" id="title" placeholder="Başlık girin">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="title">Açıklama</label>
+                                        <input type="text" name="description" class="form-control" id="description" placeholder="Açıklama girin">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="title">Renk</label>
+                                        <input type="color" name="color" class="form-control" id="color" value="#007bff">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="title">Başlangıç tarihi</label>
+                                            <div class="row">
+                                                <input type="date" name="start_date" class="form-control col-8" id="start_date" />
+                                                <input type="time" name="start_date_time" class="form-control col-4" id="start_date_time" />
+
+                                            </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="title">Bitiş tarihi</label>
+                                        <div class="row">
+                                            <input type="date" name="end_date" class="form-control col-8" id="end_date" />
+                                            <input type="time" name="end_date_time" class="form-control col-4" id="end_date_time" />
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" name="submit" value="1" class="btn btn-primary">Add</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.col-md-6 -->
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+
+    <!-- Main Footer -->
+   <?php view('static/footer') ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?= assets('plugins/jquery/jquery.min.js')?>"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= assets('plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+<script src="<?= assets('plugins/sweetalert2/sweetalert2.all.js')?>"></script>
+<!-- AdminLTE App -->
+<script src="<?= assets('js/adminlte.min.js')?>"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.2/axios.min.js" integrity="sha512-JSCFHhKDilTRRXe9ak/FJ28dcpOJxzQaCd3Xg8MyF6XFjODhy/YMCM8HW0TFDckNHWUewW+kfvhin43hKtJxAw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+
+    const todo = document.getElementById("todo")
+
+    todo.addEventListener('submit',(e)=>{
+
+        let title = document.getElementById('title').value;
+        let description = document.getElementById('description').value;
+        let category_id = document.getElementById('category_id').value;
+        let color = document.getElementById('color').value;
+        let start_date = document.getElementById('start_date').value;
+        let end_date = document.getElementById('end_date').value;
+        let start_date_time = document.getElementById('start_date_time').value;
+        let end_date_time = document.getElementById('end_date_time').value;
+
+        let formatDate = new FormData();
+
+        formatDate.append('title',title)
+        formatDate.append('description',description)
+        formatDate.append('category_id',category_id)
+        formatDate.append('color',color)
+        formatDate.append('start_date',start_date)
+        formatDate.append('end_date',end_date)
+        formatDate.append('start_date_time',start_date_time)
+        formatDate.append('end_date_time',end_date_time)
+
+        axios.post('<?=url('api/addtodo') ?>',formatDate).then(res=>{
+
+            if (res.data.redirect){
+                window.location.href = res.data.redirect;
+            }else {
+                Swal.fire({
+                    title:res.data.title,
+                    text:res.data.msg,
+                    icon:res.data.status
+                })
+            }
+
+
+            console.log(res)
+        }).catch(err=>console.log(err))
+
+        e.preventDefault();
+    })
+
+</script>
+
+</body>
+</html>
